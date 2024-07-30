9bb3ebb3a9f78fd1ec16e55e6cc40c90c16d788c
Preston Arnold <prestonarnold29@gmail.com>
2024-07-30 16:18:22 +0100

    Whoops!
    Leaked my keys

--- src/helpers/spotifyClient.ts
+++ src/helpers/spotifyClient.ts
@@ -1,15 +1,15 @@
+// GENERATE USING:
+// https://alecchen.dev/spotify-refresh-token/
+// (too lazy to make a full auth system for this)
+
 import SpotifyWebApi from "spotify-web-api-node";
 
 export const spotifyClient = new SpotifyWebApi({
-  clientId: "b2735dde628d486da407b9a457d30efa",
-  clientSecret: "d9362807a92341d08516b3e91b01a306",
+  clientId: "----",
+  clientSecret: "----",
 });
 
-let accessToken =
-  "BQBd3ivg6kIMipxksYFBH1lE5wOinzqWZhRRseCnC2P17vlmuLYP-XjgJX0yiexrtG3NVfXqHwwS0O5LTZdYt3oAafyGReg0XM3l8g6uHW9pYM4VhyHRZ1fcQ0CrcHW4X37TiqaP-BWZK3czkgK2hVsCoZewO1KvRxUj4-27vMmwRB9OsnmQg7JhImrZPj6FNkaKwywSTRvXiiMCiTtSEWXPU_dSCDkKpLZwWHuDyXwEzUYP8YDF6Ubh5T9mXVpDGYZJqiEbh_fiENuvE6q8r76yrNK9Bk8zsR9Mw3G9Y8wcbeA3378akQbHe4aDVWdGyh2rxaSpcr8E-xdA0zKx1WT3HA";
-const refreshToken =
-  "AQDhV5DphD0A_xr5rxkbsEitS3gBmzBKCSZt9-8m04MQOJz_L4DuPBe0Rc3nPKBMRW8O4Kjea199MoNrgXQ8cFFoYt54BUgMQZUNiPoSZaShuZc-R5Vz6sIKDfnLP1YVchs";
-
+let accessToken = "----";
 const setAccessToken = (token: string) => {
   spotifyClient.setAccessToken(token);
 };
